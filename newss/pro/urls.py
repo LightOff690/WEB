9d299e4fee8d42f49f1cb3a7c6a354bfe89d3135
LightOff690 <139847828+LightOff690@users.noreply.github.com>
2023-12-09 22:54:41 +0300

    Update urls.py

--- newss/pro/urls.py
+++ newss/pro/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pro_home, name='pro_home')
+    path('', views.pro_home, name='pro_home'),
+    path('pro_create', views.pro_create, name='pro_create')
 ]
