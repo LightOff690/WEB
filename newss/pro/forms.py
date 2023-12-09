a9f1b2445480e49ace9ce4fb9e87eb9095e2f20c
LightOff690 <139847828+LightOff690@users.noreply.github.com>
2023-12-09 22:55:16 +0300

    Add files via upload

---
+++ newss/pro/forms.py
@@ -0,0 +1,29 @@
+from .models import Articles
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['tittle', 'anons', 'full_text', 'date']
+
+        widgets = {
+            "tittle": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder':'Название статьи'
+
+            }),
+            "anons": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+
+            }),
+            "date": DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Дата публикации'
+            }),
+            "full_text": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текст статьи'
+            }),
+        }
