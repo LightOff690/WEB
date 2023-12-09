5920b289accd5c86173babeb8c6b289de64fea8c
LightOff690 <139847828+LightOff690@users.noreply.github.com>
2023-12-09 22:54:23 +0300

    Update views.py

--- newss/pro/views.py
+++ newss/pro/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from.models import Articles
+from .forms import ArticlesForm
 def pro_home(request):
     pro = Articles.objects.order_by('-date')
     return render(request, 'pro/pro_home.html', {'pro': pro})
+
+def pro_create(request):
+    error = ''
+    if request.method =='POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error='Форма была неверной'
+
+    form = ArticlesForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'pro/pro_create.html', data)
