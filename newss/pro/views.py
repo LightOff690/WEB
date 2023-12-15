3120fd0ebc05fdb350583d647b3b419e05401db0
LightOff690 <139847828+LightOff690@users.noreply.github.com>
2023-12-16 01:11:08 +0300

    Update views.py

--- newss/pro/views.py
+++ newss/pro/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from.models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
+
 def pro_home(request):
     pro = Articles.objects.order_by('-date')
     return render(request, 'pro/pro_home.html', {'pro': pro})
 
+class ProDetailView(DetailView):
+    model = Articles
+    template_name = 'pro/details_view.html'
+    context_object_name = 'article'
 def pro_create(request):
     error = ''
     if request.method =='POST':
