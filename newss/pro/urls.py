05ccb5f3e786e5484ab8a9cceefb24c97a1b7e0a
LightOff690 <139847828+LightOff690@users.noreply.github.com>
2024-01-03 02:41:05 +0300

    Update urls.py

--- newss/pro/urls.py
+++ newss/pro/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.pro_home, name='pro_home'),
     path('pro_create', views.pro_create, name='pro_create'),
-    path('<int:pk>', views.ProDetailView.as_view(), name='pro-detail')
-]
+    path('<int:pk>', views.ProDetailView.as_view(), name='pro-detail'),
+    path('<int:pk>/update', views.ProUpdateView.as_view(), name='pro-update'),
+    path('<int:pk>/delete', views.ProDeleteView.as_view(), name='pro-delete')
+    ]
+
