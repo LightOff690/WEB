52b011a8a8034dfde4d54f687863c413d0fd1f15
LightOff690 <139847828+LightOff690@users.noreply.github.com>
2024-01-03 02:40:45 +0300

    Update views.py

--- newss/pro/views.py
+++ newss/pro/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from.models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView,UpdateView, DeleteView
 
 def pro_home(request):
     pro = Articles.objects.order_by('-date')
@@ -11,6 +11,15 @@ class ProDetailView(DetailView):
     model = Articles
     template_name = 'pro/details_view.html'
     context_object_name = 'article'
+class ProUpdateView(UpdateView):
+    model = Articles
+    template_name = 'pro/pro_create.html'
+    from_class = ArticlesForm
+
+class ProDeleteView(DeleteView):
+    model = Articles
+    success_url = '/pro'
+    template_name = 'pro/pro_delete.html'
 def pro_create(request):
     error = ''
     if request.method =='POST':
